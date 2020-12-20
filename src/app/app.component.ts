c705d3c55db06514297d55d136b320a8461df51f
georgianft <chaganavageorge@gmail.com>
2020-12-20 13:53:19 +0400

    Dark Mode added branch giorgi
    Dark Mode added branch giorgi
    
    https://stackblitz.com/edit/dark-mode-with-angular-and-css-variables?file=angular.json
    
    export const routingModule: ModuleWithProviders<unknown> = RouterModule.forRoot(
      routes
    );

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ThemeService } from '../theme/theme.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HackathonProject';
+  constructor(private themeService: ThemeService) {}
+
+  toggle() {
+    const active = this.themeService.getActiveTheme() ;
+    if (active.name === 'light') {
+      this.themeService.setTheme('dark');
+    } else {
+      this.themeService.setTheme('light');
+    }
+  }
 }
